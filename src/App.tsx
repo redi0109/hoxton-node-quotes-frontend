947847c9614ff2ff69cf35b6c000db3c2370d9a7
Demir Boraj <demirboraj97@gmail.com>
2022-08-31 00:45:35 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 type Quote = {
-  
-}
+  id: number;
+  quote: string;
+  author: string;
+};
 
 function App() {
-  const [quotes, setQuotes] = useState(0)
+  const [quotes, setQuotes] = useState<Quote[]>([]);
 
-  return (
-    <div className="App">
-        
-    </div>
-  )
+  useEffect(() => {
+    fetch(`http://localhost:4000/quotes`)
+    .then((res) => res.json())
+    .then((quotesFromServer) => setQuotes(quotesFromServer));
+  }, [])
+
+  return <div className="App">
+     <ul>
+        <h1>Quotes</h1>
+        {quotes.map((quote) => (
+          <li key={quote.id}>
+            <h4>{quote.quote}</h4>
+            <h2>{quote.author}</h2>
+          </li>
+        ))}
+      </ul>
+  </div>;
 }
 
-export default App
+export default App;
