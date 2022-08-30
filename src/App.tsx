9b4c623e52c8e3f88c656e6255a6eb3cc7e8d9ea
Demir Boraj <demirboraj97@gmail.com>
2022-08-31 00:42:04 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+type Quote = {
+  
+}
+
+function App() {
+  const [quotes, setQuotes] = useState(0)
+
+  return (
+    <div className="App">
+        
+    </div>
+  )
+}
+
+export default App
